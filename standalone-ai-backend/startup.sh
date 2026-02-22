6fe9836701fb8b82568bf228a34aa6ed52ee3dcb
Ragavendra Rajamanickam <ragavjee.r@gmail.com>
2026-02-22 01:40:46 +0100

    fix deployment: update startup.sh paths and azure startup command

--- standalone-ai-backend/startup.sh
+++ standalone-ai-backend/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd standalone-ai-backend
+echo "Starting boot script at $(pwd)..."
 # In some Azure base images, global pip commands without --user fail silently
 python -m pip install --upgrade pip
 python -m pip install --user -r requirements.txt
 export PATH="$HOME/.local/bin:$PATH"
+echo "Starting uvicorn..."
 python -m uvicorn main:app --host 0.0.0.0 --port 8000
